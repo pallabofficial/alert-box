e0050a79a884ca4bb86b48a76a54ee56e8331ce3
pallabofficial <officialpallabmondal000@gmail.com>
2024-03-06 18:28:55 +0530

    final code snippet

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+// -------------------------------------------------------alert box--------------------------------------
+
+let screenWidth= window.innerWidth;
 const btn = document.querySelector("#test");
 const alertBox = document.querySelector("#alert-box");
 const alertOverlay = document.querySelector("#alert-box-overlay");
 const closeAlertBtn = document.querySelector("#alert-btn");
 
 btn.addEventListener("click", (event)=>{
-    alertBox.style.top = "50vh";
+    
+    if(screenWidth>769 && window.innerHeight<=screenWidth){
+        alertBox.style.top = "2vh";        
+    }
+    else{
+        alertBox.style.top = "50vh";        
+    }    
     alertOverlay.style.top = 0;
 });
 
